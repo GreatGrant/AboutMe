ad367e8eb647959021d83f9619b83a3ffd5cd79a
GreatGrant <greatgrant116@gmail.com>
2023-01-20 08:42:42 -0800

    Add onClick action to button

--- app/src/main/java/com/gralliams/aboutme/MainActivity.kt
+++ app/src/main/java/com/gralliams/aboutme/MainActivity.kt
@@ -2,10 +2,25 @@ package com.gralliams.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener { addNickName(it) }
+    }
+
+    private fun addNickName(it: View?) {
+        val nickname = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameDisplay = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameDisplay.text = nickname.text
+        nickname.visibility = View.INVISIBLE
+        nicknameDisplay.visibility = View.VISIBLE
     }
 }
