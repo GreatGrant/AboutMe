d3f02ff6823e006da146e2f3371e1737cadcbc93
GreatGrant <greatgrant116@gmail.com>
2023-01-20 08:45:28 -0800

    Add code to hide Keyboard

--- app/src/main/java/com/gralliams/aboutme/MainActivity.kt
+++ app/src/main/java/com/gralliams/aboutme/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.gralliams.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -12,7 +14,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.button).setOnClickListener { addNickName(it) }
+        findViewById<Button>(R.id.button).setOnClickListener {
+            addNickName(it)
+            // Hide the keyboard.
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(it.windowToken, 0)
+        }
     }
 
     private fun addNickName(it: View?) {
